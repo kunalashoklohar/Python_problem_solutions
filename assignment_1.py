69f59cded08898a2c73ec621e51fc7c3d9b45248
kunalashoklohar <91951708+kunalashoklohar@users.noreply.github.com>
2021-10-18 20:44:26 +0530

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -2,3 +2,18 @@
 # Write a program doing follwing things:
 # User should input his/her name
 # a msg should get printed on the console "Welcome kunal.
+
+# Solution
+quarters = []
+for year in range(2019,now.year +1):
+#     print(year)
+    if(year == now.year):
+        print("Watch it",year)
+        for q in range(1,math.ceil(now.month/3) +1):
+            
+            quarters.append(f"Q{q} {year}")
+    else:    
+        for q in range(1,5):
+            print(f"Q{q} {year}")
+            quarters.append(f"Q{q} {year}")
+print(quarters)  
